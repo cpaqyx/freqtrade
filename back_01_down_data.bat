bb2c5926218327fb49ad23b285ad224a2ceb0eb9
chenpeng <cpaqyx@aliyun.com>
2026-01-04 20:18:25 +0800

    清理文件

--- back_01_down_data.bat
+++ back_01_down_data.bat
@@ -4,5 +4,5 @@ e:
 cd E:\git\freqtrade\freqtrade\
 
 ::c:\users\administrator\.conda\envs\python312\python.exe ./freqtrade/main.py download-data -c ./user_data/config_backtesting.json --days 90 --timeframes 5m 1h
-c:\users\administrator\.conda\envs\python312\python.exe ./freqtrade/main.py download-data -c ./user_data/UpDownTrend.json --timerange 20250720-20250814 --timeframes 5m 1h
+::c:\users\administrator\.conda\envs\python312\python.exe ./freqtrade/main.py download-data -c ./user_data/UpDownTrend.json --timerange 20250720-20250814 --timeframes 5m 1h
 pause
